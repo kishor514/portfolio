c2bfb61aadc0a6936866f7e604517c120d486074
kishor514 <140046240+kishor514@users.noreply.github.com>
2026-06-23 02:37:28 -0700

    Update animated-background-config.ts

--- src/components/animated-background-config.ts
+++ src/components/animated-background-config.ts
@@ -9,7 +9,7 @@ export const STATES = {
     },
     mobile: {
       scale: { x: 0.18, y: 0.18, z: 0.18 },
-      position: { x: 0, y: -150, z: 0 },
+      position: { x: 0, y: -50, z: 0 },
       rotation: { x: 0, y: 0, z: 0 },
     },
   },
